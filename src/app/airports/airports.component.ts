b7cc8031ec2e7c037201f422e517484bd3c2b8c5
Tin <tin.vaiti@ericssonnikolatesla.com>
2024-03-09 19:26:49 +0100

    create fetch airports from the api in airports service

--- src/app/airports/airports.component.ts
+++ src/app/airports/airports.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AirportsService } from '../services/airports.service';
+import { SSRCheck } from '../shared/SSRCheck';
+import { Airports } from '../model/Airports';
 
 @Component({
   selector: 'app-airports',
   standalone: true,
   imports: [],
   templateUrl: './airports.component.html',
-  styleUrl: './airports.component.css'
+  styleUrl: './airports.component.css',
 })
-export class AirportsComponent {
+export class AirportsComponent extends SSRCheck implements OnInit {
+  airportsService = inject(AirportsService);
 
+  ngOnInit() {
+    if (SSRCheck.hasSSRRendered()) {
+      this.airportsService.getAirports('berlin').subscribe({
+        next: (airports: Airports) => {
+          console.log(airports);
+        },
+        error: (error) => {
+          console.log('Error while fetching airports', error);
+        },
+      });
+    }
+  }
 }
--- src/app/services/airports.service.ts
+++ src/app/services/airports.service.ts
@@ -1,22 +1,24 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { API } from '../../environments/environment';
 import { Observable } from 'rxjs';
 import { TokenResponse } from '../model/TokenResponse';
+import { Airports, LocationSubType } from '../model/Airports';
+import { SSRCheck } from '../shared/SSRCheck';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AirportsService {
-  http = inject(HttpClient);
+export class AirportsService extends SSRCheck {
+  private http = inject(HttpClient);
 
   getBearerToken(): Observable<TokenResponse> {
-    let body = new URLSearchParams();
+    const body = new URLSearchParams();
     body.set('grant_type', 'client_credentials');
     body.set('client_id', API.key);
     body.set('client_secret', API.secret);
 
-    let heraders = new HttpHeaders({
+    const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded',
     });
 
@@ -24,8 +26,23 @@ export class AirportsService {
       `/api/v1/security/oauth2/token`,
       body,
       {
-        headers: heraders,
+        headers: headers,
       }
     );
+  }
+
+  getAirports(input: string): Observable<Airports> {
+    const params = new HttpParams()
+      .set('keyword', input)
+      .set('subType', LocationSubType.AIRPORT);
+
+    const headers = new HttpHeaders({
+      Authorization: `${localStorage.getItem('Authorization')}`,
+    });
+
+    return this.http.get<Airports>('/api/v1/reference-data/locations', {
+      params,
+      headers,
+    });
   }
 }
