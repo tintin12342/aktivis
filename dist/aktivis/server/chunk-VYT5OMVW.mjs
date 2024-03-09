f6f45595d636b493769328e52c4c9c8702a00343
Tin <tin.vaiti@ericssonnikolatesla.com>
2024-03-09 10:31:53 +0100

    first commit

---
+++ dist/aktivis/server/chunk-VYT5OMVW.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as r,b as i,c as p,j as n,l as m,m as f,p as s,t as a,w as c}from"./chunk-EK45ZB36.mjs";var d=(()=>{let o=class o{};o.\u0275fac=function(e){return new(e||o)},o.\u0275cmp=r({type:o,selectors:[["app-root"]],standalone:!0,features:[p],decls:1,vars:0,template:function(e,y){e&1&&i(0,"router-outlet")},dependencies:[a]});let t=o;return t})();var u=[];var l={providers:[c(u),f()]};var C={providers:[s()]},v=n(l,C);var x=()=>m(d,v),z=x;export{z as a};
---
+++ dist/aktivis/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-VYT5OMVW.mjs";import"./chunk-EK45ZB36.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
