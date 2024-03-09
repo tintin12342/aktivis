2b73da3abd0ddd2606b473b8e1574959ebfc1a37
Tin <tin.vaiti@ericssonnikolatesla.com>
2024-03-09 10:49:19 +0100

    add Airports component and routing

---
+++ src/app/airports/airports.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airports',
+  standalone: true,
+  imports: [],
+  templateUrl: './airports.component.html',
+  styleUrl: './airports.component.css'
+})
+export class AirportsComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AirportsComponent } from './airports/airports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: AirportsComponent },
+    { path: '**', redirectTo: '' }
+];
