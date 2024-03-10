83b81a1d1c206ac0bed8af30ff1123d7b8d21a52
Tin <tin.vaiti@ericssonnikolatesla.com>
2024-03-10 09:52:52 +0100

    build v1.1.0

--- dist/aktivis/server/main.server.mjs
+++ dist/aktivis/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VYT5OMVW.mjs";import"./chunk-EK45ZB36.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-DGTDPUFP.mjs";import"./chunk-U6RFWWYA.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
