59a4414a87e98740ae57b1bb7bf25b3d25970577
Tin <tin.vaiti@ericssonnikolatesla.com>
2024-03-10 13:52:43 +0100

    add airport details component design and functionality

--- src/app/airport-details/airport-details.component.ts
+++ src/app/airport-details/airport-details.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { AirportDetails } from '../model/AirportDetails';
+import { AirportsService } from '../services/airports.service';
+import { SSRCheck } from '../shared/SSRCheck';
 
 @Component({
   selector: 'app-airport-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './airport-details.component.html',
-  styleUrl: './airport-details.component.css'
+  styleUrl: './airport-details.component.css',
 })
-export class AirportDetailsComponent {
+export class AirportDetailsComponent extends SSRCheck implements OnInit {
+  airportsService = inject(AirportsService);
+  route = inject(ActivatedRoute);
 
+  id: string | undefined = undefined;
+  airportDetails: AirportDetails | undefined = undefined;
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.id = params['id'];
+
+      if (this.id && SSRCheck.hasSSRRendered()) {
+        const data = localStorage.getItem(this.id);
+        
+        if (data) {
+          this.airportDetails = JSON.parse(data);
+        } else {
+          this.airportsService
+            .getAirportDetails(this.id)
+            .subscribe((details: AirportDetails) => {
+              this.airportDetails = details;
+            });
+        }
+      }
+    });
+  }
+
+  onSubmit(formGroup: NgForm) {
+    const formData = formGroup.value;
+    if (SSRCheck.hasSSRRendered() && this.airportDetails) {
+      let editedForm: AirportDetails = {
+        ...this.airportDetails,
+      };
+      editedForm.data.name = formData.name;
+      editedForm.data.address.cityName = formData.cityName;
+      editedForm.data.address.countryName = formData.countryName;
+      localStorage.setItem(editedForm.data.id, JSON.stringify(editedForm));
+    }
+  }
 }
